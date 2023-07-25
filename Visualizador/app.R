3eb4901feb5f6a4d0f67d10e944b5a25971ade54
pecaifus <pecaifus@alumni.uv.es>
2023-07-25 12:39:46 +0200

    Creación de la versión interactiva de los 4 principales gráficos con plotly y redistribución del código

--- Visualizador/app.R
+++ Visualizador/app.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(shinyFiles)
 library(zoo)
 library(shinythemes)
+library(plotly)
 
 ui <- fluidPage(
     theme = shinytheme("darkly"),    
@@ -21,18 +22,17 @@ ui <- fluidPage(
                      h1("Gráfico sobre las frecuencias cardiacas
                         actividad uterina"),
                      h2("Frecuencia cardicaca fetal y materna"),
-                     plotOutput("HR"),
+                     plotlyOutput("HR"),
                      h2("Actividad uterina"),
-                     plotOutput("TOCO")),
+                     plotlyOutput("TOCO")),
             tabPanel("SPO2",
                      h1("Gráfico sobre el oxígeno en sangre"),
-                     plotOutput("SPO2")),
+                     plotlyOutput("SPO2")),
             tabPanel("Presion",
                      h1("Gráfico sobre las diferentes presiones"),
-                     plotOutput("PRESION")),
+                     plotlyOutput("PRESION")),
             tabPanel("Datos analógicos", 
-                     plotOutput("ana"),
-                     textOutput("cabeceraAna"))
+                     plotOutput("ana"))
           )
         )
     )
@@ -41,7 +41,9 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  # volumes <- getVolumes()() # this makes the directory at the base of your computer.
+  ## CARGA DE DATOS ##
+  
+  # volumes <- getVolumes()()
   volumes <- c(MonitorizacionFetal = "C:/Users/perem/OneDrive/Escritorio/Practicas_2023/MonitorizacionFetal")
   
   shinyDirChoose(input, 'folder', roots=volumes)
@@ -51,8 +53,242 @@ server <- function(input, output) {
     ficheros <- list.files(dir_path)
     return(list(dir_path = dir_path, ficheros = ficheros))
   })
+
   
+  # Cargamos los datos del fichero digital
+  datosDig <- reactive({
+    
+    fichero <- directorio()$ficheros[4]
+    dir_path <- directorio()$dir_path
+    datapath <- paste(dir_path, fichero, sep = "/")
+    
+    filename <- file(
+      description = datapath,
+      open = "rb")
+    
+    data <- readBin(filename, integer(),
+                            n = file.info(datapath)$size,
+                            size = 1, signed = FALSE)
+    
+    HR1 <- data[seq(1, length(data), by = 9)]
+    HR2 <- data[seq(2, length(data), by = 9)]
+    MHR <- data[seq(3, length(data), by = 9)]
+    TOCO <- data[seq(4, length(data), by = 9)]
+    SPO2 <- data[seq(5, length(data), by = 9)]
+    VCP <- data[seq(6, length(data), by = 9)]
+    Psistolica <- data[seq(7, length(data), by = 9)]
+    Pdiastolica <- data[seq(8, length(data), by = 9)]
+    Pmedia <- data[seq(9, length(data), by = 9)]
+    
+    data <- cbind(HR1, HR2, MHR, TOCO, SPO2, VCP,
+                  Psistolica, Pdiastolica, Pmedia)
+    
+    return(data)
+  })
   
+  # Filtramos los datos digitales por las señales seleccionadas
+  digitalesFiltrados <- reactive({
+    datosDig <- datosDig()
+    seleccionada <- input$señal
+    filtrado <- subset(datosDig, select = seleccionada)
+    return(filtrado)
+  })
+  
+  # Cargamos los datos del fichero analógico
+  datosAna <- reactive({
+    
+    fichero <- directorio()$ficheros[3]
+    dir_path <- directorio()$dir_path
+    datapath <- paste(dir_path, fichero, sep = "/")
+    
+    filename <- file(
+      description = datapath,
+      open = "rb")
+    
+    data <- readBin(filename, integer(),
+                    n = file.info(datapath)$size,
+                    size = 2, signed = TRUE)
+    return(data)
+  })
+  
+  ## CREACIÓN DE GRÁFICOS ##
+  
+  # Almacenamos las señales seleccionadas por el usuario
+  output$señales <- renderUI({
+    validate(
+      need(input$folder, message = "Esperando fichero digital")
+    )
+    
+    datosDig <- datosDig()
+    selectizeInput("señal", label = "Señales dataset",
+                choices = colnames(datosDig), multiple = TRUE)
+  })
+  
+  # Mostramos los normbres de las señales seleccionadas
+  output$nombre_señal <- renderText({
+    return(input$señal)
+  })
+  
+  # Creamos el gráfico de HR1 y MHR
+  output$HR <- renderPlotly({
+    validate(
+      need(input$folder, message = "Esperando fichero digital")
+    )
+    
+    df <- as.data.frame(datosDig())
+    df <- df %>% dplyr::select(HR1, HR2, MHR)
+    
+    # Revisar si está bien hecho el cambio a tiempo
+    seg <- nrow(df) / 4000
+    df$x <- seq(0, seg, length.out = nrow(df))
+    
+    df <- df %>% pivot_longer(names_to = "Variable",
+                              values_to = "Valor", cols = !x)
+    
+    # Revisar este filtrado de datos
+    # df <- df %>% filter((Valor > 50 & Variable == "HR1") |
+    #                     (Valor > 50 & Variable == "MHR"))
+
+    # ventana <- 20
+    # df <- df %>%
+    #       group_by(Variable) %>%
+    #       summarise(Suavizado = rollmean(Valor, k = ventana, fill = NA),
+    #                 Tiempo = x)
+    # 
+    # ggplot(df, aes(x = Tiempo, y = Suavizado, color = Variable)) +
+    #   geom_line() +
+    #   labs(x = "Tiempo (s)", y = "Valor")
+    
+    g1 <- plot_ly(data = df, type = "scatter", mode = "lines") %>%
+      add_trace(x = ~ x, y = ~ Valor, color = ~ Variable) %>%
+      layout(yaxis = list(title = "Latidos por minuto")) %>%
+      config(scrollZoom = TRUE)
+
+    g1
+  })
+
+  # Creamos el gráfico de TOCO
+  output$TOCO <- renderPlotly({
+    validate(
+      need(input$folder, message = "Esperando fichero digital")
+    )
+    
+    TC <- as.data.frame(datosDig())
+    TC <- TC %>% dplyr::select(TOCO)
+    seg <- nrow(TC) / 4000 # Revisar si está bien hecho el cambio a tiempo
+    TC$x <- seq(0, seg, length.out = nrow(TC))
+    
+    ventana <- 20
+    TC <- TC %>% 
+      mutate(TOCO = rollmean(TOCO, k = ventana, fill = NA))
+    
+    # ggplot(TC, aes(x = x, y = TOCO)) + 
+    #   geom_line(color = "green") +
+    #   labs(x = "Tiempo (s)", y = "Valor (mmHg)")
+    
+    g1 <- plot_ly(data = TC, type = "scatter",
+                  mode = "lines") %>% 
+      add_trace(x = ~ x, y = ~ TOCO,
+                line = list(color = "green"), name = "TOCO") %>% 
+      config(scrollZoom = TRUE) %>% 
+      layout(yaxis = list(title = "Valor (mmHg)"),
+             xaxis = list(title = "Tiempo (s)"))
+    
+    g1
+  })
+  
+  # Creamos el gráfico del SPO2
+  output$SPO2 <- renderPlotly({
+    validate(
+      need(input$folder, message = "Esperando fichero digital")
+    )
+    
+    df <- as.data.frame(datosDig())
+    df <- df %>% dplyr::select(SPO2)
+    
+    seg <- nrow(df) / 4000
+    df$x <- seq(0, seg, length.out = nrow(df))
+    
+    df <- df %>% filter(SPO2 > 0)
+    
+    
+    # ggplot(df, aes(x = x, y = SPO2)) +
+    #   geom_line(color = "blue") + labs(x = "Tiempo (s)", y = "Porcentaje %")
+    
+    g1 <- plot_ly(data = df, type = "scatter", mode = "lines") %>% 
+      add_trace(x = ~ x, y = ~ SPO2, color = "green") %>% 
+      layout(yaxis = list(title = "Porcentaje %"),
+             xaxis = list(title = "Tiempo (s)")) %>% 
+      config(scrollZoom = TRUE)
+    
+    g1
+  })
+  
+  # Creamos el gráfico de las presiones
+  output$PRESION <- renderPlotly({
+    validate(
+      need(input$folder, message = "Esperando fichero digital")
+    )
+    
+    df <- as.data.frame(datosDig())
+    df <- df %>% dplyr::select(Pmedia, Pdiastolica, Psistolica)
+    
+    seg <- nrow(df) / 4000
+    df$x <- seq(0, seg, length.out = nrow(df))
+    
+    df_l <- df %>% 
+      pivot_longer(names_to = "Variable", values_to = "Valor", cols = !x)
+    
+    # ggplot(data = df_l ,aes(x = x, y = Valor, color = Variable)) +
+    #   geom_line() +
+    #   labs(x = "Tiempo (s)", y = "Valor mmHg")
+    
+    g1 <- plot_ly(data = df_l, type = "scatter", mode = "lines") %>% 
+      add_trace(x = ~ x, y = ~ Valor, color = ~ Variable) %>% 
+      config(scrollZoom = TRUE) %>% 
+      layout(yaxis = list(title = "Valor mmHg"),
+             xaxis = list(title = "Tiempo (s)"))
+    
+    g1
+  })
+  
+  # Creamos el gráfico de las señales a representar
+  output$digi <- renderPlot({
+    
+    validate(
+      need(input$folder, message = "Esperando fichero digital")
+    )
+    validate(
+      need(input$señal, message = "Esperando señal a representar")
+    )
+    
+    # Trabajamos los datos para poder representar las variables juntas
+    señal <- data.frame(digitalesFiltrados())
+    x <- seq(1, dim(señal)[1])
+    datos <- cbind(x, señal)
+    datos <- datos %>% 
+      pivot_longer(cols = -x, names_to = "señal", values_to = "valor")
+    
+    # Creamos el gráfico
+    ggplot(data = datos, aes(x = x, y = valor)) + 
+      geom_line(aes(color = señal)) + facet_wrap(~señal) + theme_bw()
+    
+  })
+  
+  # Creamos el gráfico del fichero analógico
+  output$ana <- renderPlot({
+    validate(
+      need(input$folder, message = "Esperando fichero analógico")
+    )
+    
+    datosAna <- datosAna()
+    plot(tail(datosAna, 500))
+  })
+
+  ###############  
+  ## CABECERAS ##
+  ###############
+    
   # Cargamos la cabecera del fichero digital
   cabeceraDigital <- reactive({
     
@@ -113,200 +349,6 @@ server <- function(input, output) {
     resolucion <- as.numeric(datos[7])
     print(paste("La fecha es: ", fecha))
     print(paste("La resolucion es: ", resolucion))
-  })
-
-  # Cargamos los datos del fichero digital
-  datosDig <- reactive({
-    
-    fichero <- directorio()$ficheros[4]
-    dir_path <- directorio()$dir_path
-    datapath <- paste(dir_path, fichero, sep = "/")
-    
-    filename <- file(
-      description = datapath,
-      open = "rb")
-    
-    data <- readBin(filename, integer(),
-                            n = file.info(datapath)$size,
-                            size = 1, signed = FALSE)
-    
-    HR1 <- data[seq(1, length(data), by = 9)]
-    HR2 <- data[seq(2, length(data), by = 9)]
-    MHR <- data[seq(3, length(data), by = 9)]
-    TOCO <- data[seq(4, length(data), by = 9)]
-    SPO2 <- data[seq(5, length(data), by = 9)]
-    VCP <- data[seq(6, length(data), by = 9)]
-    Psistolica <- data[seq(7, length(data), by = 9)]
-    Pdiastolica <- data[seq(8, length(data), by = 9)]
-    Pmedia <- data[seq(9, length(data), by = 9)]
-    
-    data <- cbind(HR1, HR2, MHR, TOCO, SPO2, VCP,
-                  Psistolica, Pdiastolica, Pmedia)
-    
-    return(data)
-  })
-  
-  # Almacenamos las señales seleccionadas por el usuario
-  output$señales <- renderUI({
-    validate(
-      need(input$folder, message = "Esperando fichero digital")
-    )
-    
-    datosDig <- datosDig()
-    selectizeInput("señal", label = "Señales dataset",
-                choices = colnames(datosDig), multiple = TRUE)
-  })
-  
-  # Mostramos los normbres de las señales seleccionadas
-  output$nombre_señal <- renderText({
-    return(input$señal)
-  })
-  
-  # Filtramos los datos digitales por las señales seleccionadas
-  digitalesFiltrados <- reactive({
-    datosDig <- datosDig()
-    seleccionada <- input$señal
-    filtrado <- subset(datosDig, select = seleccionada)
-    return(filtrado)
-  })
-  
-  # Creamos el gráfico de HR1 y MHR
-  output$HR <- renderPlot({
-    validate(
-      need(input$folder, message = "Esperando fichero digital")
-    )
-    
-    df <- as.data.frame(datosDig())
-    df <- df %>% dplyr::select(HR1, HR2, MHR)
-    
-    # Revisar si está bien hecho el cambio a tiempo
-    seg <- nrow(df) / 4000
-    df$x <- seq(0, seg, length.out = nrow(df))
-    
-    df <- df %>% pivot_longer(names_to = "Variable",
-                              values_to = "Valor", cols = !x)
-    
-    # Revisar este filtrado de datos
-    # df <- df %>% filter((Valor > 50 & Variable == "HR1") |
-    #                     (Valor > 50 & Variable == "MHR"))
-
-    ventana <- 20
-    df <- df %>%
-          group_by(Variable) %>%
-          summarise(Suavizado = rollmean(Valor, k = ventana, fill = NA),
-                    Tiempo = x)
-    ggplot(df, aes(x = Tiempo, y = Suavizado, color = Variable)) +
-      geom_line() +
-      labs(x = "Tiempo (s)", y = "Valor")
-  })
-
-  # Creamos el gráfico de TOCO
-  output$TOCO <- renderPlot({
-    validate(
-      need(input$folder, message = "Esperando fichero digital")
-    )
-    
-    TC <- as.data.frame(datosDig())
-    TC <- TC %>% dplyr::select(TOCO)
-    seg <- nrow(TC) / 4000 # Revisar si está bien hecho el cambio a tiempo
-    TC$x <- seq(0, seg, length.out = nrow(TC))
-    
-    ventana <- 20
-    TC <- TC %>% 
-      mutate(TOCO = rollmean(TOCO, k = ventana, fill = NA))
-    
-    ggplot(TC, aes(x = x, y = TOCO)) + 
-      geom_line(color = "green") +
-      labs(x = "Tiempo (s)", y = "Valor (mmHg)")
-  })
-  
-  # Creamos el gráfico del SPO2
-  output$SPO2 <- renderPlot({
-    validate(
-      need(input$folder, message = "Esperando fichero digital")
-    )
-    
-    df <- as.data.frame(datosDig())
-    df <- df %>% dplyr::select(SPO2)
-    
-    seg <- nrow(df) / 1000
-    df$x <- seq(0, seg, length.out = nrow(df))
-    
-    df <- df %>% filter(SPO2 > 0)
-    
-    
-    ggplot(df, aes(x = x, y = SPO2)) +
-      geom_line(color = "blue") + labs(x = "Tiempo (s)", y = "Porcentaje %")
-  })
-  
-  # Creamos el gráfico de las presiones
-  output$PRESION <- renderPlot({
-    validate(
-      need(input$folder, message = "Esperando fichero digital")
-    )
-    
-    df <- as.data.frame(datosDig())
-    df <- df %>% dplyr::select(Pmedia, Pdiastolica, Psistolica)
-    
-    seg <- nrow(df) / 1000
-    df$x <- seq(0, seg, length.out = nrow(df))
-    
-    df_l <- df %>% 
-      pivot_longer(names_to = "Variable", values_to = "Valor", cols = !x)
-    
-    ggplot(data = df_l ,aes(x = x, y = Valor, color = Variable)) +
-      geom_line() +
-      labs(x = "Tiempo (s)", y = "Valor mmHg")
-  })
-  
-  # Creamos el gráfico de las señales a representar
-  output$digi <- renderPlot({
-    
-    validate(
-      need(input$folder, message = "Esperando fichero digital")
-    )
-    validate(
-      need(input$señal, message = "Esperando señal a representar")
-    )
-    
-    # Trabajamos los datos para poder representar las variables juntas
-    señal <- data.frame(digitalesFiltrados())
-    x <- seq(1, dim(señal)[1])
-    datos <- cbind(x, señal)
-    datos <- datos %>% 
-      pivot_longer(cols = -x, names_to = "señal", values_to = "valor")
-    
-    # Creamos el gráfico
-    ggplot(data = datos, aes(x = x, y = valor)) + 
-      geom_line(aes(color = señal)) + facet_wrap(~señal) + theme_bw()
-    
-  })
-  
-  # Cargamos los datos del fichero analógico
-  datosAna <- reactive({
-    
-    fichero <- directorio()$ficheros[3]
-    dir_path <- directorio()$dir_path
-    datapath <- paste(dir_path, fichero, sep = "/")
-    
-    filename <- file(
-      description = datapath,
-      open = "rb")
-    
-    data <- readBin(filename, integer(),
-                    n = file.info(datapath)$size,
-                    size = 2, signed = TRUE)
-    return(data)
-  })
-  
-  # Creamos el gráfico del fichero analógico
-  output$ana <- renderPlot({
-    validate(
-      need(input$folder, message = "Esperando fichero analógico")
-    )
-    
-    datosAna <- datosAna()
-    plot(tail(datosAna, 500))
   })
 
 }
